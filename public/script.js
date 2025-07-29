ea4c8a804f6684f8c5461a474c67c381a156f14c
hajin0chung <hajinchung1@gmail.com>
2025-07-29 11:10:28 +0000

    fix redirect search param

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const qs = (selector) => document.querySelector(selector);
 
 function main() {
   const urlParams = new URLSearchParams(location.search);
-  const redirectURI = urlParams.get("redirect_uri") ?? "https://monitor.deps.me";
+  const redirectURI = urlParams.get("redirect_to") ?? "https://monitor.deps.me";
   qs("#submit").onclick = async () => {
     const password = qs("#password-input").value;
     const res = await fetch("/login", {
