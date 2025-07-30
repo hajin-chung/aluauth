28b427ece8e976388cedd0ed32cb329fc5ba3214
hajin0chung <hajinchung1@gmail.com>
2025-07-30 06:07:47 +0000

    styling stuff

--- public/script.js
+++ public/script.js
@@ -1,20 +1,26 @@
 const qs = (selector) => document.querySelector(selector);
 
-function main() {
+async function handleSubmit() {
   const urlParams = new URLSearchParams(location.search);
   const redirectURI = urlParams.get("redirect_to") ?? "https://monitor.deps.me";
-  qs("#submit").onclick = async () => {
-    const password = qs("#password-input").value;
-    const res = await fetch("/login", {
-      method: "POST",
-      body: password,
-    });
-    if (res.status == 200) {
-      location.href = redirectURI;
-    } else {
-      console.log("retry");
-    }
+
+  const password = qs("#password-input").value;
+  const res = await fetch("/login", {
+    method: "POST",
+    body: password,
+  });
+  if (res.status == 200) {
+    location.href = redirectURI;
+  } else {
+    qs("#password-input").classList.add("wrong");
   }
+}
+
+function main() {
+  qs("#password-input").addEventListener("keydown", (evt) => {
+    if (evt.key === "Enter") handleSubmit();
+  });
+  qs("#submit").onclick = handleSubmit;
 }
 
 main()
